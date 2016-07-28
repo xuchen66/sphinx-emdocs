cd05dc247a6cfd76a47f70d794020aea9369987e
xuchen66 <xuchen66@gmail.com>
2016-07-27 21:22:39 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Cryo-EM related docs'
+project = u'CHEN'S CORNER'
 copyright = u'2016, Chen Xu'
 author = u'Chen Xu'
 
