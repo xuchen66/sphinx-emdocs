9d83ffd4c9794c80f38080823d94393c211c14dd
xuchen66 <xuchen66@gmail.com>
2016-07-28 14:30:27 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -144,7 +144,7 @@ html_theme = 'classic'
 # of the sidebar.
 #
 # html_logo = None
-html_logo = image/Umass-CryoEM.png
+html_logo = images/Umass-CryoEM.png
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
