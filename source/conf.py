c109665093946132ac9ac2686ef4ed7362fa4d9d
xuchen66 <xuchen66@gmail.com>
2019-08-23 21:58:53 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 # General information about the project.
 # project = u'Cryo-EM related docs'
 project = u'Umass Cryo-EM Docs'
-copyright = u'2016, 2017 Umass Cryo-EM Facility'
+copyright = u'2016, 2017, 2018, 2019 Umass Cryo-EM Facility'
 author = u'UMass CryoEM Core Facilty'
 
 # The version info for the project you're documenting, acts as replacement for
