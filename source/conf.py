eb07152bdcb2e20f8cad1fd6bb36c1c5178ca3e5
xuchen66 <xuchen66@gmail.com>
2021-04-08 19:42:42 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -122,7 +122,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'classic'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
