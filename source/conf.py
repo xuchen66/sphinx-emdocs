a5147a3bb7b90c605fbe30b40eaee7b4a8333cf7
xuchen66 <xuchen66@gmail.com>
2021-04-08 20:05:30 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -121,7 +121,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+#html_theme = 'classic'
+html_theme = 'sphinxdoc'
 #html_theme = 'sphinx_rtd_theme'
 #html_theme = 'alabaster'
 
