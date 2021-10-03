dbf6549db8faf11c43d82ce717f659a8250a1302
xuchen66 <xuchen66@gmail.com>
2021-10-03 12:23:04 -0400

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,8 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [sphinx_rtd_theme,
+extensions = [
+     'sphinx_rtd_theme',
 # other extensions
 ]
 
